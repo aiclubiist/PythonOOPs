b5fb5a002df0dc84da37c04334f1f7ebba2a7bb6
kurmananda <edu.kurmananda@gmail.com>
2025-11-16 18:49:14 +0530

    changing encoding language to utf-8

--- fetchSatelliteData.py
+++ fetchSatelliteData.py
@@ -3,7 +3,7 @@ data = requests.get('https://db.satnogs.org/api/satellites/')
 
 # print(data.json())
 
-satFile = open('satelliteData.json', 'w')
+satFile = open('satelliteData.json', 'w', encoding='utf-8')
 satFile.write(data.text)
 satFile.close()
 
